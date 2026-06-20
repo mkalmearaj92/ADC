768f3756399270c37e86be64e0b0705c789949c3
mkalmearaj92 <mkalmearaj92@gmail.com>
2026-06-20 18:37:39 +0100

    created two structures to store data

--- adc.h
+++ adc.h
@@ -4,5 +4,25 @@
 
 #ifndef ADC_ADC_H
 #define ADC_ADC_H
+#include <stdint.h>
 
+typedef struct {
+    float timestamp;
+    uint8_t channelID;
+    uint16_t raw_value; //adc
+    int16_t temperature; //could be negative
+    uint8_t status_flags;
+    uint32_t sequence_number;
+    uint8_t reserved[2]; //array of reserved
+}AdcSample;
+
+typedef struct {
+    uint32_t magic;
+    uint16_t version;
+    uint16_t channel_count;
+    uint32_t sample_count;
+    uint32_t sample_rate;
+    uint8_t reserved[8];
+}Header;
 #endif //ADC_ADC_H
+
