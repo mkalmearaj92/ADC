d72a4b1f023c37f68148fca46fcf9811ce6937a7
mkalmearaj92 <mkalmearaj92@gmail.com>
2026-07-01 16:19:25 +0100

    Start with channel stats , create Stats structer , create stats function header

--- stats.c
+++ stats.c
@@ -3,3 +3,6 @@
 //
 
 #include "stats.h"
+void calculate_stats(AdcSample * records , uint32_t sample_count , Stats ch_st[4] ) {
+    
+}
--- stats.h
+++ stats.h
@@ -4,5 +4,14 @@
 
 #ifndef ADC_STATS_H
 #define ADC_STATS_H
+#include "adc.h"
 
+typedef struct {
+   float min_voltage;
+   float max_voltage;
+   float average; //mean
+   float std_dev;
+}Stats;
+
+void calculate_stats(AdcSample * records , uint32_t sample_count , Stats ch_st[4] );
 #endif //ADC_STATS_H
